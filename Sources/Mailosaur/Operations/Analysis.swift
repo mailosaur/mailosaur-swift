0a1e6b739b25519602f26eee7544be4980d105b2
ey-mailosaur <ethan.y@mailosaur.com>
2024-09-27 10:43:49 +0100

    Introduced deliverability reporting request/models

--- Sources/Mailosaur/Operations/Analysis.swift
+++ Sources/Mailosaur/Operations/Analysis.swift
@@ -33,4 +33,25 @@ public class Analysis {
             throw error
         }
     }
+
+    /// Perform a deliverability report of an email.
+    ///
+    /// - Parameter email: The identifier of the message to be analyzed.
+    public func deliverabilityResult(email: String) async -> Result<DeliverabilityReport, Error> {
+        guard let client = self.client else { return .failure(MailosaurError.clientUninitialized) }
+        return await client.performRequest(path: "api/analysis/deliverability/\(email)")
+    }
+
+    /// Perform a deliverability report of an email.
+    ///
+    /// - Parameter email: The identifier of the message to be analyzed.
+    public func deliverability(email: String) async throws -> DeliverabilityReport {
+        let result = await self.deliverabilityResult(email: email)
+        switch result {
+        case .success(let value):
+            return value
+        case .failure(let error):
+            throw error
+        }
+    }
 }
